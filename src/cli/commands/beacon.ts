dc1ca34719d0f396651663315a0c078c9aa196fc
Gregory Markou <gregorymarkou@gmail.com>
2019-05-19 00:53:35 -0400

    for cayman

--- src/cli/commands/beacon.ts
+++ src/cli/commands/beacon.ts
@@ -14,6 +14,7 @@ import * as RPCApis from "../../rpc/api";
 import deepmerge from "deepmerge";
 import {getTomlConfig, IConfigFile} from "../../util/toml";
 import defaults from "../../node/defaults";
+import * as C from "../../constants";
 
 interface IBeaconCommandOptions {
   db: string;
